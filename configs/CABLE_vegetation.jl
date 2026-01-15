b7db1d3a140c7a04ddd2540d866b916de4a5c794
Whyborn <lachlan.s.whyborn@gmail.com>
2026-01-15 12:37:57 +1100

    Add start of a config file

---
+++ configs/CABLE_vegetation.jl
@@ -0,0 +1,16 @@
+@PFT EvergreenBroadleaf phenology=evergreen
+@PFT DeciduousBroadleaf phenology=deciduous
+@PFT EvergreenNeedleleaf phenology=evergreen
+@PFT DeciduousNeedleleaf phenology=deciduous
+@PFT Shrub phenology=deciduous
+@PFT C3Grass phenology=deciduous
+@PFT C4Grass phenology=evergreen
+
+PFT_ids = (EvergreenBroadleaf => 2,
+               DeciduousBroadleaf => 4,
+               EvergreenNeedleleaf => 1,
+               DeciduousNeedleleaf => 3,
+               Shrub => 5,
+               C3Grass => 6,
+               C4Grass => 7
+              )
